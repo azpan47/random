2d27798a05e402a46daaf0bf75977d54c9c7dffc
Pavan Pemmaraju <azpan47@gmail.com>
2015-09-02 02:16:54 -0400

    longfact

---
+++ goprog/longfact2.py
@@ -0,0 +1,21 @@
+import sys, os
+
+n = int(input("Enter the number < 200 :"))
+r = [1]
+
+def prod(r, i):
+	c = 0
+	for j in range(len(r)):
+			t = r[j]*i + c
+			r[j] = t%10
+			c = t/10
+
+	while(c):
+			rm = c%10
+			c = c/10
+			r.append(rm)
+
+for i in range(2,n+1):
+		prod(r, i)
+
+print r
